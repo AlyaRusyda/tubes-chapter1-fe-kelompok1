2886acdee95c322af8c1ccc9b633d469a267d879
Alya Rusyda Maharistya <100183012+AlyaRusyda@users.noreply.github.com>
2022-12-19 23:06:44 +0700

    Update calculator.jsx

--- src/pages/calculator.jsx
+++ src/pages/calculator.jsx
@@ -1,141 +1,175 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from "react";
+import { btns, BTN_ACTIONS } from "./ButtonConfig";
+import Navbar from "./navbar";
+import Footer from "./footer";
+import Sun from "../icons/Sun";
+import Moon from "../icons/Moon";
+import "../css/calculator.css"
 
-import '../css/calculator.css';
-import Navbar from './navbar';
-import Footer from './footer';
-import { btns, BTN_ACTIONS } from './ButtonConfig';
+function Calculator() {
+  const btnsRef = useRef(null);
+  const expRef = useRef(null);
 
+  const [expression, setExpression] = useState("");
+  const operation = ["+", "-", "/", "*"];
 
-const Calculator = () => {
+  useEffect(() => {
+    const btns = Array.from(btnsRef.current.querySelectorAll("button"));
+    btns.forEach((e) => (e.style.height = e.offsetWidth + "px"));
+  }, []);
 
-    const btnsRef = useRef(null);
-    const expRef = useRef(null);
+  const btnClick = (item) => {
+    let expDiv = expRef.current;
 
-    const [expression, setExpression] = useState('');
+    if (item.action === BTN_ACTIONS.THEME)
+      document.body.classList.toggle("dark");
 
-    useEffect(() => {
-        const btns = Array.from(btnsRef.current.querySelectorAll('button'));
-        btns.forEach(e => e.style.height = e.offsetWidth + 'px');
-    }, []);
+    if (item.action === BTN_ACTIONS.ADD) {
+      const op = item.display !== "x" ? item.display : "*";
+      const value = expression.split("");
 
-    const btnClick = (item) => {
-        const expDiv = expRef.current;
+      addAnimSpan(expression + op);
 
-        if (item.action === BTN_ACTIONS.THEME) document.body.classList.toggle('dark');
+      if (operation.includes(value[value.length - 1]) && op === "%") {
+        addAnimSpan(expression);
+        return;
+      }
 
-        if (item.action === BTN_ACTIONS.ADD) {
-            addAnimSpan(item.display);
+      if (
+        operation.includes(value[value.length - 1]) &&
+        operation.includes(op)
+      ) {
+        value.splice(value.length - 1, 1, op);
+        let result = value.join("");
 
-            const oper = item.display !== 'x' ? item.display : '*';
-            setExpression(expression + oper);
-        }
-
-        if (item.action === BTN_ACTIONS.DELETE) {
-            expDiv.parentNode.querySelector('div:last-child').innerHTML = '';
-            expDiv.innerHTML = '';
-
-            setExpression('');
-        }
-
-        if (item.action === BTN_ACTIONS.PERCENT) {
-            let output = Number(expression) / 100;
-            console.log(output);
-            setExpression(output)
-            expDiv.innerHTML = output;
-        }
-
-        if (item.action === BTN_ACTIONS.DEL) {
-            let output = expression.split('');
-            output.pop()
-            let result = output.join('');
-            console.log(result)
-
-            expDiv.innerHTML = result;
-            setExpression(result)
-        }
-
-        if (item.action === BTN_ACTIONS.CALC) {
-            if (expression.trim().length <= 0) return;
-
-            expDiv.parentNode.querySelector('div:last-child').remove();
-
-            const cloneNode = expDiv.cloneNode(true);
-            expDiv.parentNode.appendChild(cloneNode);
-
-            const transform = `translateY(${-(expDiv.offsetHeight + 10) + 'px'}) scale(0.4)`;
-
-            try { 
-
-                let res = eval(expression);
-
-                setExpression(res.toString());
-                setTimeout(() => {
-                    cloneNode.style.transform = transform;
-                    expDiv.innerHTML = '';
-                    addAnimSpan(Math.floor(res * 100000000) / 100000000);
-                }, 200);
-            } catch {
-                setTimeout(() => {
-                    cloneNode.style.transform = transform;
-                    cloneNode.innerHTML = 'Syntax err';
-                }, 200);
-            } finally {
-                console.log('calc complete');
-            }
-        }
+        setExpression(expression);
+        addAnimSpan(result);
+        return;
+      }
     }
 
-    const addAnimSpan = (content) => {
-        const expDiv = expRef.current;
-        const span = document.createElement('span');
+    if (item.action === BTN_ACTIONS.DELETE) {
+      expDiv.parentNode.querySelector("div:last-child").innerHTML = "";
+      expDiv.innerHTML = "";
 
-        span.innerHTML = content;
-        span.style.opacity = '0';
-        expDiv.appendChild(span);
+      setExpression("");
+    }
 
-        const width = span.offsetWidth + 'px';
-        span.style.width = '0';
+    if (item.action === BTN_ACTIONS.DEL) {
+      let output = expression.split("");
+      output.pop();
+      let result = output.join("");
 
+      expDiv.innerHTML = result;
+      setExpression(result);
+    }
+
+    if (item.action === BTN_ACTIONS.CALC) {
+      if (expression.trim().length <= 0) return;
+
+      expDiv.parentNode.querySelector("div:last-child").remove();
+
+      const cloneNode = expDiv.cloneNode(true);
+      expDiv.parentNode.appendChild(cloneNode);
+
+      const transform = `translateY(${
+        -(expDiv.offsetHeight + 10) + "px"
+      }) scale(0.4)`;
+
+      try {
+        let value = "";
+        if (expression.includes("%")) {
+          value = expression.replace("%", "/100");
+        } else {
+          value = expression;
+        }
+
+        let res = eval(value);
+
+        setExpression(res.toString());
         setTimeout(() => {
-            span.style.opacity = '1';
-            span.style.width = width;
-        }, 100);
+          cloneNode.style.transform = transform;
+          expDiv.innerHTML = "";
+          addAnimSpan(Math.floor(res * 100000000) / 100000000);
+        }, 200);
+      } catch {
+        setTimeout(() => {
+          cloneNode.style.transform = transform;
+          cloneNode.innerHTML = "Syntax err";
+        }, 200);
+      } finally {
+        console.log("calc complete");
+      }
     }
+  };
 
-    return (
-        <>
-        
-            <div className='all'>
-                 <Navbar/>
-              
-                <div className="calculator">
-                    <div className="calculator__result">
-                        <div ref={expRef} className="calculator__result__exp"></div>
-                        <div className="calculator__result__exp"></div>
-                    </div>
-                    <div ref={btnsRef} className="calculator__btns">
-                        {
-                            btns.map((item, index) => (
-                                <button
-                                    key={index}
-                                    className={item.class}
-                                    onClick={() => btnClick(item)}
-                                >
-                                    {item.display}
-                                </button>
-                            ))
-                        }
-                    </div>
+  const addAnimSpan = (content) => {
+    const expDiv = expRef.current;
+    const span = document.createElement("span");
 
-                </div>
-                </div>
-                <br/>
-            <Footer/>
-            
+    expDiv.innerHTML = "";
+    span.innerHTML = content;
+    span.style.opacity = "0";
+    expDiv.appendChild(span);
 
-        </>
+    setExpression(content.toString());
 
-    );
+    const width = span.offsetWidth + "px";
+    span.style.width = "0";
+
+    setTimeout(() => {
+      span.style.opacity = "1";
+      span.style.width = width;
+    }, 100);
+  };
+
+  // THEME
+  const [isToggled, setIsToggled] = useState(false)
+  const [theme, setTheme] = useState('dark');
+  
+  const onToggle = () => {
+    setIsToggled(!isToggled);
+    console.log(theme)
+    if (theme === 'dark') {
+        document.body.classList.toggle(theme);
+    }
+  };
+
+
+  return (
+    <>
+    <Navbar />
+      <div className="all">
+        <div className="switch">
+            <Sun />
+            <label className="toggle-switch">
+                <input type="checkbox" checked={isToggled} onChange={onToggle} />
+                <span className="switch" />
+            </label>
+            <Moon />
+        </div>
+        <div className="calculator">
+          <div className="calculator__result">
+            <div ref={expRef} className="calculator__result__exp"></div>
+            <div className="calculator__result__exp"></div>
+          </div>
+          <div ref={btnsRef} className="calculator__btns">
+            {btns.map((item, index) => (
+              <button
+                key={index}
+                className={item.class}
+                onClick={() => btnClick(item)}
+              >
+                {item.display}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+      <br/>
+      <Footer/>
+    </>
+  );
 }
 
 export default Calculator;
