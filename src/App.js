a7521d6720045d351b72923686ca51e067864d35
Ice <100953845+aisyakareninaa@users.noreply.github.com>
2022-12-20 23:37:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Navbar from "./pages/navbar";
 import Index from "./pages/index";
 import Calkulator from "./pages/calculator";
 import Footer from "./pages/footer";
@@ -9,7 +8,6 @@ function App() {
     <div className>
       <Router>
         <Routes>
-          <Route path="/navbar" element={<Navbar />} />
           <Route path="/" element={<Index />} />
           <Route path="/calculator" element={<Calkulator />} />
           <Route path="/footer" element={<Footer />} />
